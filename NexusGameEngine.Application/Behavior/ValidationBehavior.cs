5ec7ba65823c8d2f6019c636664b3b6cb0456bec
Gabriele Gullì <magapano89@gmail.com>
2026-08-17 14:51:18 +0200

    Feat: MediatR & Pipeline Behavior with Fluentvalidation init
    - Added MediatR with declaration in Application DependencyInjection file
    - Created ValidationBehavior class for Pipeline Behavior
    - Added Fluentvalidation in pipeline and in DependencyInjection file for searching all validation Classes in assembly

---
+++ NexusGameEngine.Application/Behavior/ValidationBehavior.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using NexusGameEngine.Domain.ResultPattern;
+
+namespace NexusGameEngine.Application.Behavior;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators, ILogger<ValidationBehavior<TRequest, TResponse>> log)
+: IPipelineBehavior<TRequest, TResponse>
+where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!validators.Any()) return await next(cancellationToken);
+
+        var context = new ValidationContext<TRequest>(request);
+        var validationResult = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResult.SelectMany(r => r.Errors)
+                        .Where(f => f != null)
+                        .ToList();
+
+        if (failures.Count == 0) return await next(cancellationToken);
+
+        var errorDetails = failures.Select(f => $"{f.PropertyName}: {f.ErrorMessage}").ToArray();
+        var validationErrors = Error.Validation("Request.Validation", "Request contains validation errors", errorDetails);
+
+        if (typeof(TResponse).IsGenericType && typeof(TResponse).GetGenericTypeDefinition() == typeof(Result<>))
+        {
+            var valueType = typeof(TResponse).GetGenericArguments();
+            var resultType = typeof(Result<>).MakeGenericType(valueType);
+
+            var failureMethod = resultType.GetMethod("Failure", BindingFlags.Public | BindingFlags.Static);
+            if (failureMethod is not null)
+            {
+                return (TResponse)failureMethod.Invoke(null, [new List<Error> { validationErrors }])!;
+            }
+        }
+        else if (typeof(TResponse) == typeof(ResultBase))
+        {
+            return (TResponse)(object)ResultBase.Failure([validationErrors]);
+        }
+
+        throw new ValidationException(failures);
+    }
+
+}
--- NexusGameEngine.Application/DependencyInjection.cs
+++ NexusGameEngine.Application/DependencyInjection.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NexusGameEngine.Application;
@@ -6,6 +8,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssemblies(assembly);
+            configuration.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
         return services;
     }
 }
