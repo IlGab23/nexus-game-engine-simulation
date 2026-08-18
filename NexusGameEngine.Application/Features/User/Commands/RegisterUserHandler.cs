a831f8586d23f5c4dc21ca724d86174b44193c4f
Gabriele Gullì <magapano89@gmail.com>
2026-08-18 02:24:43 +0200

    Feat: Registration Command and Handler with Validation
    - Added RegisterUserCommand e Output DTO
    - Added Validation Rules with fluent validation
    - Added RegisterUserHandler to handler registration
    - Removed gitTraceFile

---
+++ NexusGameEngine.Application/Features/User/Commands/RegisterUserHandler.cs
@@ -0,0 +1,30 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NexusGameEngine.Application.Interfaces;
+using NexusGameEngine.Domain.Entities.ValueObjects;
+using NexusGameEngine.Domain.ResultPattern;
+using DomainUser = NexusGameEngine.Domain.Entities.User;
+
+namespace NexusGameEngine.Application.Features.User.Commands;
+
+
+public class RegisterUserHandler(IApplicationDbContext appDbContext) : IRequestHandler<RegisterUserCommand, Result<RegistrationOutput>>
+{
+    public async Task<Result<RegistrationOutput>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        if (await appDbContext.Users.AnyAsync(u => u.Email.Value == request.Email, cancellationToken)) return Error.RegistrationEmailExist;
+        var emailResult = Email.Create(request.Email);
+        if (emailResult.IsFailure) return emailResult.ErrorList;
+
+        string hashedPassword = request.Password; //Hashing System: W.I.P.
+
+        var userResult = DomainUser.Create(request.UserName, emailResult.Value, hashedPassword);
+        if (userResult.IsFailure) return userResult.ErrorList;
+
+        await appDbContext.Users.AddAsync(userResult.Value, cancellationToken);
+        await appDbContext.SaveChangesAsync(cancellationToken);
+
+        return new RegistrationOutput(userResult.Value.Id);
+    }
+
+}
