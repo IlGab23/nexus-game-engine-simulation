71127ea5affb1813090775d4df569e249e11812e
Gabriele Gullì <magapano89@gmail.com>
2026-09-16 10:11:24 +0200

    fix: Add validation for Player commands
    - Added ClaimDailyRewardCommandValidator to ensure PlayerId is not empty
    
    - Added UseSpecialSkillCommandValidator to ensure PlayerId is not empty

--- NexusGameEngine.Application/Features/Player/Commands/ClaimDailyRewardCommand.cs
+++ NexusGameEngine.Application/Features/Player/Commands/ClaimDailyRewardCommand.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using MediatR;
 using NexusGameEngine.Domain.ResultPattern;
 
@@ -6,3 +7,13 @@ namespace NexusGameEngine.Application.Features.Player.Commands;
 public record ClaimDailyRewardCommand(Guid PlayerId) : IRequest<Result<DailyRewardResponse>>;
 
 public record DailyRewardResponse(int? MoneyGained, string? ItemNameGained, int? ItemAmountGained);
+
+public class ClaimDailyRewardCommandValidator : AbstractValidator<ClaimDailyRewardCommand>
+{
+    public ClaimDailyRewardCommandValidator()
+    {
+        RuleFor(x => x.PlayerId)
+            .NotEmpty().WithMessage("Player Id cannot be empty");
+
+    }
+}
--- NexusGameEngine.Application/Features/Player/Commands/UseSpecialSkillCommand.cs
+++ NexusGameEngine.Application/Features/Player/Commands/UseSpecialSkillCommand.cs
@@ -1,6 +1,16 @@
+using FluentValidation;
 using MediatR;
 using NexusGameEngine.Domain.ResultPattern;
 
 namespace NexusGameEngine.Application.Features.Player.Commands;
 
 public record UseSpecialSkillCommand(Guid PlayerId) : IRequest<Result<bool>>;
+
+public class UseSpecialSkillCommandValidator : AbstractValidator<UseSpecialSkillCommand>
+{
+    public UseSpecialSkillCommandValidator()
+    {
+        RuleFor(x => x.PlayerId)
+            .NotEmpty().WithMessage("Player Id cannot be empty");
+    }
+}
